fe13c4468d71972ae8f0f9094504ec82e011dc81
SamreenJagdev <104173786+SamreenJagdev@users.noreply.github.com>
2022-08-18 21:35:52 -0400

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,9 +1,11 @@
 def sum(num1,num2){
   sum=num1+num2
   echo "Sum of number1 and number2 is ${sum}"
+}
  
  def multiply(num1,num2){
    mul=num1*num2
    echo "Multiplication of two numbers is ${mul}"
+ }
    
     
